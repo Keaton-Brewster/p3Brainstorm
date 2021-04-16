359315fd8a4520147bd0fb443f797ac7d8245fc6
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-16 15:50:55 -0500

    just changed the way the spotifyApi class got exported out of the config file so that is an error happens in the creating of the spotify api node class, it can be logged more effectively

--- config/config.js
+++ config/config.js
@@ -1,10 +1,16 @@
 const blotify = require('./blotify');
 const SpotifyAPI = require('spotify-web-api-node');
+const spotifyApi = new SpotifyAPI({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_TOKEN,
+});
 
+//! Changed the way this module.export works, so yea going to want to make sure that it works before commiting this
 module.exports = () => {
-    return new SpotifyAPI({
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        redirectUri: process.env.REDIRECT_TOKEN,
-    })
+    try {
+        return spotifyApi
+    } catch (e) {
+        throw new Error(e);
+    }
 }
